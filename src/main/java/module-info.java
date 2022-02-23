45d9566d0a51935f55f717467be1a8ebba114ffc
Calin Catalin <71898535+catalincc4@users.noreply.github.com>
2022-02-24 01:23:57 +0200

    new

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module org.example {
 
     opens org.example to javafx.fxml;
     exports org.example;
+    exports org.example.controller;
+    opens org.example.controller to javafx.fxml;
 }
---
+++ src/main/java/org/example/controller/Controller.java
@@ -0,0 +1,211 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField answerField;
+
+    @FXML
+    private Button delButton;
+
+    @FXML
+    private Button derivationButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private TextField firstPolynomial;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button integrationButton;
+
+    @FXML
+    private Button minusButton;
+
+    @FXML
+    private Button multiplicateButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button plusButton;
+
+    @FXML
+    private Button pointButton;
+
+    @FXML
+    private Button risePowerButton;
+
+    @FXML
+    private TextField secondPolynomial;
+
+    @FXML
+    private Button seventButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button slashButton;
+
+    @FXML
+    private Button starButton;
+
+    @FXML
+    private Button subtractButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button xButton;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    void oneButtonAction(MouseEvent event) {
+
+    }
+    @FXML
+    void twoButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void threeButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fourButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fiveButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sixButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sevenButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void eightButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void nineButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void zeroButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void slashButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void pointButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void plusButtonAction(MouseEvent event) {
+
+    }
+
+
+    @FXML
+    void starButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void xButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void minusButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void risePowerButtonAction(MouseEvent event) {
+
+    }
+    @FXML
+    void delButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void divideButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void multiplicateButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void subtractButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void derivationButtonAction(MouseEvent event) {
+        System.out.println("derivation button ");
+    }
+
+    @FXML
+    void integrationButtonAction(MouseEvent event) {
+        System.out.println("Integration button ");
+    }
+
+}
